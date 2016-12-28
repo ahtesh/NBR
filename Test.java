75d0a403d9c570bbf85ec44ea3b7f62285775fb9
Ahtesh Inamdar <ahtesh.inamdar@in.abb.com>
2016-12-28 12:39:15 +0530

    add files into github

---
+++ Test.java
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Ant passed");
+	}
+
+}
+=======
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Ant passed");
+	}
+
+}
+>>>>>>> ccc2b7617e4cd4fa5057b7f67b7c1e498ad9cc25
